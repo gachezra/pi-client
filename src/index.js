c85e38e1f08ffd69dd4df70128d92fbeb3245fc4
gachezra <gachomoezra@gmail.com>
2024-11-14 18:23:43 +0300

    wtf is normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
 import './defaults.css';
 import Shop from './Shop';
 import reportWebVitals from './reportWebVitals';
