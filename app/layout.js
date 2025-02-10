c79052b4851a2ebb9a5875081b58063eb16d9be9
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-02-10 09:12:50 +0000

    Tired

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,12 @@
-import Script from 'next/script'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script src="https://sdk.minepi.com/pi-sdk.js" />
-        <Script id="pi-sdk-init">
-          {`
-            window.Pi ? window.Pi.init({ 
-              version: "2.0", 
-              sandbox: process.env.NODE_ENV !== 'production'
-            }) : null;
-          `}
-        </Script>
+        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+        <script>
+          Pi.init({ version: "2.0", sandbox: true });
+        </script>`
       </head>
       <body>{children}</body>
     </html>
